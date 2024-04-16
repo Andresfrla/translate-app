bba77ae39d629c0c52822f48024b78972c36091e
unknown <andresfranla@gmail.com>
2024-04-16 16:47:08 -0600

    fix all responsive styles

--- app/page.js
+++ app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
     <main className="bg-[url('../public/hero_img.jpg')] bg-contain bg-[#040711] bg-no-repeat w-[100%] h-screen flex flex-col items-center">
       <header className="mt-[100px] md:mb-[0px]"><img src='/logo.svg' alt="logo" className="md:h-14"/></header>
       <div className="flex flex-col items-center xl:grid xl:grid-cols-2">
-        <div className="mt-[60px] bg-[#212936cc] rounded-3xl border border-[#4D5562] w-[590px] h-[340px] md:w-[880px] md:mr-[70px] md:ml-[70px] mr-[60px] ml-[60px] md:text-[20px] xl:w-[560px] xl:ml-[70px] xl:mr-[156px]">
+        <div className="mt-[60px] bg-[#212936cc] rounded-3xl border border-[#4D5562] w-[590px] h-[340px] md:w-[880px] md:h-[340px] md:mr-[70px] md:ml-[70px] mr-[60px] ml-[60px] md:text-[20px] xl:w-[560px] xl:h-[340px] xl:ml-[70px] xl:mr-[156px]">
           <div className="flex p-5 md:p-6 ml-4 md:ml-5 pb-3 full-w space-x-7 items-center text-[0.875rem] md:text-[19px] xl:text-[16px]">
             <button onClick={handleDetectLanguage} className="text-[#4D5562] font-bold">Detect Language</button>
             <button 
@@ -82,7 +82,7 @@ export default function Home() {
           <div>
             <hr className="border-t border-[#394150] md:ml-7 md:mr-7 ml-5 mr-5"/>
             <textarea 
-              className="bg-transparent w-full h-[160px] pt-6 pl-6 pr-6" 
+              className="bg-transparent w-full h-[160px] md:h-[130px] pt-6 pl-6 pr-6" 
               type="input" 
               placeholder="Hello how are you?"
               value={inputText}
@@ -91,17 +91,17 @@ export default function Home() {
             <p className="text-right mr-5 text-[#4D5562] text-[0.75rem]">{inputText.length}/500</p>
           </div>
             
-          <div className="flex justify-between m-5 mt-3 mb-5 md:ml-7 md:mr-7 ">
+          <div className="flex justify-between m-5 mt-3 mb-5 md:ml-7 md:mr-7">
             <div className="space-x-2">
-              <button className="border-[2.3px] border-[#4D5562] rounded-2xl p-[6px]"><img src='/sound_max_fill.svg' className="md:size-7 md:m-[2px]"/></button>
-              <button className="border-[2.3px] border-[#4D5562] rounded-2xl p-[6px]"><img src='/Copy.svg' className="md:size-7 md:m-[2px]"/></button>
+              <button className="border-[2.3px] border-[#4D5562] rounded-xl p-[6px]"><img src='/sound_max_fill.svg' className="md:size-7 md:m-[2px]"/></button>
+              <button className="border-[2.3px] border-[#4D5562] rounded-xl p-[6px]"><img src='/Copy.svg' className="md:size-7 md:m-[2px]"/></button>
             </div>
 
             <button onClick={translateText} className="bg-[#3662E3] rounded-xl flex p-[11.7px] pl-7 pr-7 border border-[#CDD5E0]/60"><img src='/Sort_alfa.svg' className="pr-1.5"/>Translate</button>
           </div>
         </div>
 
-        <div className="mt-5 bg-[#212936cc] rounded-3xl border border-[#4D5562] w-[590px] h-[340px] md:w-[880px] md:mr-[70px] md:ml-[70px] mr-[60px] ml-[60px] md:text-[20px] xl:w-[560px] xl:mr-[70px] xl:ml-[0px] xl:mt-[60px] xl:text-[16px]">
+        <div className="mt-5 bg-[#212936cc] rounded-3xl border border-[#4D5562] w-[590px] h-[340px] md:w-[880px] md:mr-[70px] md:ml-[70px] mr-[60px] ml-[60px] md:text-[20px] xl:w-[560px] xl:h-[340px] xl:mr-[70px] xl:ml-[0px] xl:mt-[60px] xl:text-[16px]">
           <div className="flex justify-between">
             <div className="flex p-5 ml-4 pb-3 full-w space-x-7 items-center text-[0.875rem] md:text-[19px] md:ml-7 md:mr-7 md:p-6 md:pt-8 xl:text-[16px]">
               <button 
@@ -127,7 +127,7 @@ export default function Home() {
           </div>
           <div>
             <hr className="border-t border-[#394150] ml-5 mr-5 md:ml-7 md:mr-7"/>
-            <p className="ml-5 mr-5 mt-7 m-36 font-semibold md:ml-7 md:mr-7 xl:pb-[50px]">{translatedText}</p>
+            <p className="ml-5 mr-5 mt-7 m-36 font-semibold md:ml-7 md:mr-7 md:mb-[110px] xl:pb-[10px]">{translatedText}</p>
           </div>
             
           <div className="flex justify-between m-5 mt-3 mb-5">
